def40537b3af4af2ecdfabab7911c4b94a4ee58b
a1qs <ankiller2007.09.20@gmail.com>
2026-07-05 03:43:31 +0200

    chore: log when skipping gen configs

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/optimizations/MixinSetupEvents.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/optimizations/MixinSetupEvents.java
@@ -4,10 +4,13 @@ import iskallia.vault.event.SetupEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import xyz.iwolfking.woldsvaults.WoldsVaults;
 
 @Mixin(value = SetupEvents.class, remap = false)
 public class MixinSetupEvents {
 
     @Redirect(method = "setupCommon", at = @At(value = "INVOKE", target = "Liskallia/vault/init/ModConfigs;registerGen()V"))
-    private static void ignoreSetupLoad() {}
+    private static void ignoreSetupLoad() {
+        WoldsVaults.LOGGER.info("Skipped loading Gen Configs for Common Setup");
+    }
 }
