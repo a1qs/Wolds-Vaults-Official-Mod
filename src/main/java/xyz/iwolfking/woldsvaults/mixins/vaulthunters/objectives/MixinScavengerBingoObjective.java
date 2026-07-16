d002d0007bc3b19d4717f1142e05358c5397c1d0
a1qs <ankiller2007.09.20@gmail.com>
2026-07-16 15:38:09 +0200

    fix: scav tag would apply to every dropped item

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/objectives/MixinScavengerBingoObjective.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/objectives/MixinScavengerBingoObjective.java
@@ -2,9 +2,12 @@ package xyz.iwolfking.woldsvaults.mixins.vaulthunters.objectives;
 
 import iskallia.vault.core.vault.Vault;
 import iskallia.vault.core.vault.objective.ScavengerBingoObjective;
+import iskallia.vault.item.BasicScavengerItem;
+import iskallia.vault.world.data.ServerVaults;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -12,10 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ScavengerBingoObjective.class, remap = false)
 public class MixinScavengerBingoObjective {
+    @Shadow
+    private boolean hasValidVaultId(ItemStack stack, Vault vault) {
+        throw new AssertionError(); // dummy
+    }
 
-    @Inject(method = "countItemFromWorld", at = @At(value = "INVOKE", target = "Liskallia/vault/core/vault/objective/ScavengerBingoObjective;get(Liskallia/vault/core/data/key/GenericFieldKey;)Ljava/lang/Object;", ordinal = 1))
+    @Inject(method = "countItemFromWorld", at = @At(value = "INVOKE", target = "Liskallia/vault/core/vault/objective/scavenger/ScavengerBingoTile;isCompleted()Z"))
     private void alwaysApplyTagWorld(ItemStack stack, CallbackInfo ci) {
-        stack.getOrCreateTag().putBoolean("ScavBingoCounted", true);
+        if (stack.getItem() instanceof BasicScavengerItem) stack.getOrCreateTag().putBoolean("ScavBingoCounted", true);
+
     }
 
     @Inject(method = "countItem", at = @At(value = "INVOKE", target = "Liskallia/vault/core/vault/objective/ScavengerBingoObjective;get(Liskallia/vault/core/data/key/GenericFieldKey;)Ljava/lang/Object;", ordinal = 1))
