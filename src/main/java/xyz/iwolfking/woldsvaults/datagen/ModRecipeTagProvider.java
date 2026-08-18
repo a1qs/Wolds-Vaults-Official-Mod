28b31d322d09bcbaeada6dbe3f9bff269e73d022
a1qs <ankiller2007.09.20@gmail.com>
2026-08-18 21:01:04 +0200

    chore: fix stuff

--- src/main/java/xyz/iwolfking/woldsvaults/datagen/ModRecipeTagProvider.java
+++ src/main/java/xyz/iwolfking/woldsvaults/datagen/ModRecipeTagProvider.java
@@ -8,8 +8,6 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 import xyz.iwolfking.woldsvaults.WoldsVaults;
-import xyz.iwolfking.woldsvaults.integration.arsnouveau.init.ArsRecipeSerializers;
-import xyz.iwolfking.woldsvaults.integration.occultism.init.OccultismRecipeSerializers;
 
 public class ModRecipeTagProvider extends TagsProvider<RecipeSerializer<?>> {
     public ModRecipeTagProvider(DataGenerator pGenerator, @Nullable ExistingFileHelper existingFileHelper) {
@@ -19,12 +17,6 @@ public class ModRecipeTagProvider extends TagsProvider<RecipeSerializer<?>> {
 
     @Override
     protected void addTags() {
-        tag(AllTags.AllRecipeSerializerTags.AUTOMATION_IGNORE.tag).addOptional(ArsRecipeSerializers.VAULT_CATALYST_INFUSION.getRegistryName());
-        tag(AllTags.AllRecipeSerializerTags.AUTOMATION_IGNORE.tag).addOptional(ArsRecipeSerializers.VAULT_GEAR_ENCHANTING_APPARATUS.getRegistryName());
-        tag(AllTags.AllRecipeSerializerTags.AUTOMATION_IGNORE.tag).addOptional(OccultismRecipeSerializers.VAULT_CRYSTAL_RITUAL.getId());
-        tag(AllTags.AllRecipeSerializerTags.AUTOMATION_IGNORE.tag).addOptional(OccultismRecipeSerializers.AUGMENT_RITUAL.getId());
-        tag(AllTags.AllRecipeSerializerTags.AUTOMATION_IGNORE.tag).addOptional(OccultismRecipeSerializers.AUGMENT_POOL_RITUAL.getId());
-        tag(AllTags.AllRecipeSerializerTags.AUTOMATION_IGNORE.tag).addOptional(OccultismRecipeSerializers.COMPANION_RITUAL.getId());
     }
 
     @Override
--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinCrystalWorkbenchContainer.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinCrystalWorkbenchContainer.java
@@ -35,7 +35,7 @@ public abstract class MixinCrystalWorkbenchContainer  extends OverSizedSlotConta
     }
     @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Liskallia/vault/container/CrystalWorkbenchContainer;addSlot(Lnet/minecraft/world/inventory/Slot;)Lnet/minecraft/world/inventory/Slot;"), remap = true,
     slice = @Slice(from = @At(value = "INVOKE", target = "Liskallia/vault/block/entity/CrystalWorkbenchTileEntity;getUniqueIngredients()Liskallia/vault/container/oversized/OverSizedInventory;", ordinal = 0),
-                     to = @At(value = "INVOKE", target = "Liskallia/vault/block/entity/CrystalWorkbenchTileEntity;getOutput()Liskallia/vault/container/oversized/OverSizedInventory;")), remap = true)
+                     to = @At(value = "INVOKE", target = "Liskallia/vault/block/entity/CrystalWorkbenchTileEntity;getOutput()Liskallia/vault/container/oversized/OverSizedInventory;")))
     private Slot stackableSeals(CrystalWorkbenchContainer instance, Slot slot, Operation<Slot> original){
         var bg = slot.getNoItemIcon();
         var typedSlot = slot instanceof CrystalWorkbenchContainer.CrystalWorkbenchSlot sl ? sl : null;
